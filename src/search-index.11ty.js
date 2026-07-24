f5f1ca063731f058afb0fb543d45472fd3167547
Lightxau <amit902861@gmail.com>
2026-07-25 00:49:02 +0530

    Add search-index module for video data

---
+++ src/search-index.11ty.js
@@ -0,0 +1,19 @@
+module.exports = class {
+  data() {
+    return {
+      permalink: "search-index.json",
+      eleventyExcludeFromCollections: true
+    };
+  }
+  render(data) {
+    const items = (data.videos || []).map(v => ({
+      id: v.id,
+      title: v.title || "Untitled",
+      type: v.type,
+      src: v.src,
+      tags: v.tags || "",
+      views: v.views || 0
+    }));
+    return JSON.stringify(items);
+  }
+};
